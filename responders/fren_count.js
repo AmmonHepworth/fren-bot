8e126202a9e79bc7c9533feb455162a53599f636
kylehovey <khoveyc@gmail.com>
2021-01-10 16:01:16 -0800

    Added esdoc for fren count

--- responders/fren_count.js
+++ responders/fren_count.js
@@ -3,11 +3,19 @@ import telegram from '../lib/telegram';
 const { WAIT_DELAY, GUILD_ID, VOICE_CHANNEL_ID } = process.env;
 
 class FrenCount {
+  /**
+   * Construct a state management instance for the fren counter
+   * @param {Object} client Discord.js Client object
+   */
   constructor(client) {
     this.client = client;
     this.disconnectTimes = {};
   }
 
+  /**
+   * Get the current member count (cached)
+   * @return {Number}
+   */
   get memberCount() {
     const { members: { size: memberCount } } = this.client
       .guilds.cache.get(GUILD_ID)
@@ -16,10 +24,21 @@ class FrenCount {
     return memberCount;
   }
 
+  /**
+   * Determine if the amount of frens is at an increment that
+   * constitutes an alert state
+   * @return {Boolean}
+   */
   get alertThresholdReached() {
     return Math.log2(this.memberCount) % 1 === 0;
   }
 
+  /**
+   * Get the time since a member joined
+   * @param {Object} member The GuildMember object returned from Discord.js
+   * @param {String} member.id The snowflake id of the member
+   * @return {Number} Milliseconds since last join
+   */
   timeSinceLastJoined({ id }) {
     return Date.now() - (this.disconnectTimes[id] || Infinity);
   }
@@ -28,11 +47,15 @@ class FrenCount {
    * Public methods
    */
 
+  /**
+   * Respond to a member joining the main voice channel
+   * @param {Object} member The GuildMember object returned from Discord.js
+   */
   onJoin(member) {
     console.log(`joined, there are now ${this.memberCount} members`);
 
     if (this.timeSinceLastJoined(member) < WAIT_DELAY) {
-      return console.log(
+      console.log(
         `member ${member.user.username} has left too recently to be counted again`,
       );
     }
@@ -42,6 +65,11 @@ class FrenCount {
     }
   }
 
+  /**
+   * Respond to a member disconnecting from the main voice channel
+   * @param {Object} member The GuildMember object returned from Discord.js
+   * @param {String} member.id The snowflake id of the member
+   */
   onDisconnect({ id }) {
     this.disconnectTimes[id] = Date.now();
 
