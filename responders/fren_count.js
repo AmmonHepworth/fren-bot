73d9e417d536a676a8f45f28985f668bd5614c8e
kylehovey <khoveyc@gmail.com>
2021-02-23 17:57:18 -0800

    Clearing timeout on cooldown removal

--- responders/fren_count.js
+++ responders/fren_count.js
@@ -16,7 +16,7 @@ class FrenCount {
   constructor(client) {
     this.client = client;
     this.disconnectTimes = {};
-    this.cooldownForCount = new Set();
+    this.cooldownForCount = new Map();
   }
 
   /**
@@ -55,11 +55,10 @@ class FrenCount {
    * @param {Number} number The number of members to put on cooldown
    */
   putOnCooldown(number) {
-    this.cooldownForCount.add(number);
-
-    setTimeout(() => {
-      this.cooldownForCount.delete(number);
-    }, ALERT_COOLDOWN);
+    this.cooldownForCount.set(
+      number,
+      setTimeout(() => this.cooldownForCount.delete(number), ALERT_COOLDOWN),
+    );
   }
 
   /**
@@ -67,8 +66,16 @@ class FrenCount {
    * @param {Number} number The lower bound above which all will be anihilated
    */
   cooldownAbove(number) {
-    this.cooldownForCount = new Set(
-      [...this.cooldownForCount].filter((n) => n < number),
+    this.cooldownForCount = new Map(
+      [...this.cooldownForCount].filter(([n, timeId]) => {
+        if (n < number) {
+          clearTimeout(timeId);
+
+          return false;
+        }
+
+        return true;
+      }),
     );
   }
 
