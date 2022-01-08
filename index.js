f2c9a41f6eee476e26d81ff6588355af8f28b23b
kylehovey <khoveyc@gmail.com>
2022-01-08 14:09:26 -0800

    Fixed inconsistent naming abstraction

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const DiscordListener = require('./lib/listeners/discord.js');
 const MinecraftListener = require('./lib/listeners/minecraft.js');
 const FrenCountResponder = require('./lib/responders/fren_count.js');
 
-const gasFrensResponder = new FrenCountResponder((frenCount) => {
+const discordResponder = new FrenCountResponder((frenCount) => {
   const plural = frenCount > 1;
   const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${plural ? 's' : ''} in the leb.`;
 
   telegram.send(msg, GAS_CHAT);
 });
 
-const gasGamerResponder = new FrenCountResponder((frenCount) => {
+const minecraftResponder = new FrenCountResponder((frenCount) => {
   const plural = frenCount > 1;
   const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${plural ? 's' : ''} on the frencraft server.`;
 
@@ -28,23 +28,23 @@ const minecraft = new MinecraftListener();
 discord.onJoin((user) => {
   console.log(`${user} has connected to Discord.`);
 
-  gasFrensResponder.onJoin(user, discord.memberCount);
+  discordResponder.onJoin(user, discord.memberCount);
 });
 
 discord.onLeave((user) => {
   console.log(`${user} has disconnected from Discord.`);
 
-  gasFrensResponder.onLeave(user, discord.memberCount);
+  discordResponder.onLeave(user, discord.memberCount);
 });
 
 minecraft.onJoin((user) => {
   console.log(`${user} has connected to Frencraft.`);
 
-  gasGamerResponder.onJoin(user, minecraft.playerCount);
+  minecraftResponder.onJoin(user, minecraft.playerCount);
 });
 
 minecraft.onLeave((user) => {
   console.log(`${user} has disconnected from Frencraft.`);
 
-  gasGamerResponder.onLeave(user, minecraft.playerCount);
+  minecraftResponder.onLeave(user, minecraft.playerCount);
 });
