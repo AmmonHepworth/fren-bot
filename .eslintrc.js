99f61390f465a6d59c16a6bffdfa5e94db9b3b64
kylehovey <khoveyc@gmail.com>
2022-07-16 20:33:42 -0700

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
   },
+  plugins: ['prettier'],
   rules: {
     // This app's only runtime output is console.log
     'no-console': 0,
@@ -19,5 +18,8 @@ module.exports = {
 
     // Without this eslint is angry at void callbacks
     'consistent-return': 0,
+
+    // Error on format
+    'prettier/prettier': 'error',
   },
 };
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 require('dotenv-flow').config();
 
-const {
-  GAS_CHAT,
-  GAS_GAMER_CHAT,
-  SM_MC_CHANNEL,
-} = process.env;
+const { GAS_CHAT, GAS_GAMER_CHAT, SM_MC_CHANNEL } = process.env;
 
 const telegram = require('./lib/senders/telegram');
 const slack = require('./lib/senders/slack');
 
-const DiscordListener = require('./lib/listeners/discord.js');
-const MinecraftListener = require('./lib/listeners/minecraft.js');
-const FrenCountResponder = require('./lib/responders/fren_count.js');
+const DiscordListener = require('./lib/listeners/discord');
+const MinecraftListener = require('./lib/listeners/minecraft');
+const FrenCountResponder = require('./lib/responders/fren_count');
 
 const discordResponder = new FrenCountResponder((frenCount) => {
   const plural = frenCount > 1;
-  const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${plural ? 's' : ''} in the leb.`;
+  const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${
+    plural ? 's' : ''
+  } in the leb.`;
 
   telegram.send(msg, GAS_CHAT);
 });
 
 const minecraftResponder = new FrenCountResponder((frenCount) => {
   const plural = frenCount > 1;
-  const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${plural ? 's' : ''} on the frencraft server.`;
+  const msg = `There ${plural ? 'are' : 'is'} ${frenCount} fren${
+    plural ? 's' : ''
+  } on the frencraft server.`;
 
   telegram.send(msg, GAS_GAMER_CHAT);
   slack.send(msg, SM_MC_CHANNEL);
