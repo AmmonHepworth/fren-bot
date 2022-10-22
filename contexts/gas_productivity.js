da33ef5f1b54d17101c2b615dad500fd7b8e5a80
kylehovey <khoveyc@gmail.com>
2022-10-22 11:32:14 -0700

    Fixed API for member count that was broken with Discord.js update

--- contexts/gas_productivity.js
+++ contexts/gas_productivity.js
@@ -1,33 +1,43 @@
 const {
-  PRODUCTIVITY_RESPONDER_ENABLED,
+  DISCORD_RESPONDER_ENABLED,
   GAS_CHAT,
   GUILD_ID,
   PRODUCTIVITY_CHANNEL_ID,
 } = process.env;
 
-const telegram = require('./lib/senders/telegram');
+const telegram = require('../lib/senders/telegram');
+
+const FrenCountResponder = require('../lib/responders/fren_count');
+const DiscordListener = require('../lib/listeners/discord');
 
 if (DISCORD_RESPONDER_ENABLED === 'true') {
   const productivityResponder = new FrenCountResponder((frenCount) => {
     const plural = frenCount > 1;
-    const msg = `${frenCount} fren${
-      plural ? 's' : ''
-    } ${plural ? 'are' : 'is'} being productive 😌.`;
+    const msg = `${frenCount} fren${plural ? 's' : ''} ${
+      plural ? 'are' : 'is'
+    } being productive 😌.`;
 
     telegram.send(msg, GAS_CHAT);
   });
 
-  const discordProductivity = new DiscordListener(GUILD_ID, PRODUCTIVITY_CHANNEL_ID);
+  const discordProductivity = new DiscordListener(
+    GUILD_ID,
+    PRODUCTIVITY_CHANNEL_ID
+  );
 
-  discordProductivity.onJoin((user) => {
+  discordProductivity.onJoin(async (user) => {
     console.log(`${user} has connected to Productive Zone.`);
 
-    productivityResponder.onJoin(user, discordProductivity.memberCount);
+    const memberCount = await discordProductivity.memberCount();
+
+    productivityResponder.onJoin(user, memberCount);
   });
 
-  discordProductivity.onLeave((user) => {
+  discordProductivity.onLeave(async (user) => {
     console.log(`${user} has disconnected from Productive Zone.`);
 
-    productivityResponder.onLeave(user, discordProductivity.memberCount);
+    const memberCount = await discordProductivity.memberCount();
+
+    productivityResponder.onLeave(user, memberCount);
   });
 }
